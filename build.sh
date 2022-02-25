cb2ba063ca5b5def95d782922fcbc829f54db8a9
cq112358 <98128829+cq112358@users.noreply.github.com>
2022-02-24 19:38:06 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -136,7 +136,7 @@ pushd build
 -prefix /opt/Qt${QT_MAJOR_VERSION}.${QT_MINOR_VERSION} \
 -qpa eglfs || exit 1
 
-make -j3 || exit 1
+make -j1 || exit 1
 
 INSTALL_ROOT=${TMPDIR} make install || exit 1
 
