3b785decbcb217d62474d3d7e18d3cd61094f4b1
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-30 18:33:14 +0100

    add screen
    to build even when ssh gets disconnected

--- buildwithlogs.sh
+++ buildwithlogs.sh
@@ -3,4 +3,4 @@ TYPE=$1
 VER2=$(git rev-parse --short HEAD) 
 echo ${VER2}
 
-./build.sh $TYPE |& tee >(ts "%d-%m-%y %H_%M_%S" > build$(date '+%m%d%H%M')-${VER2}.log)
+screen ./build.sh $TYPE |& tee >(ts "%d-%m-%y %H_%M_%S" > build$(date '+%m%d%H%M')-${VER2}.log)
