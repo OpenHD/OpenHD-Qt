9de878eecdb62aef0952c8c9c7af4a096afaf2d5
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-30 18:37:29 +0100

    syntax change

--- buildwithlogs.sh
+++ buildwithlogs.sh
@@ -3,4 +3,5 @@ TYPE=$1
 VER2=$(git rev-parse --short HEAD) 
 echo ${VER2}
 
-screen ./build.sh $TYPE |& tee >(ts "%d-%m-%y %H_%M_%S" > build$(date '+%m%d%H%M')-${VER2}.log)
+screen 
+bash build.sh $TYPE |& tee >(ts "%d-%m-%y %H_%M_%S" > build$(date '+%m%d%H%M')-${VER2}.log)
