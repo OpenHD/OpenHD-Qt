b2eeb46709a5645a1f0bb9f173b9a9dd39ac0253
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-11 01:29:17 +0100

    update qt version

--- after-install.sh
+++ after-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 touch /etc/ld.so.conf.d/qt.conf
-echo "/opt/Qt5.15.4/lib/" >/etc/ld.so.conf.d/qt.conf
+echo "/opt/Qt5.15.7/lib/" >/etc/ld.so.conf.d/qt.conf
 sudo ldconfig
-export PATH="$PATH:/opt/Qt5.15.4/bin/"
+export PATH="$PATH:/opt/Qt5.15.7/bin/"
 cd /usr/bin
 rm -f qmake
-sudo ln -s /opt/Qt5.15.4/bin/qmake qmake
+sudo ln -s /opt/Qt5.15.7/bin/qmake qmake
